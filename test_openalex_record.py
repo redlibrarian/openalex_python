5f4ba923d6b3b2163eb71e90e2d4b3178dd70d86
Sam Popowich <spopowich@gmail.com>
2025-03-17 13:20:11 -0500

    Make note why test failing.

--- test_openalex_record.py
+++ test_openalex_record.py
@@ -11,5 +11,5 @@ def test_fetch_author():
 def test_fetch_keywords():
     assert oar.fetch_keywords(item) == ['biology', 'cell biology', 'computational biology', 'gene', 'genetics', 'saccharomyces cerevisiae', 'transformation (genetics)', 'yeast']
 
-def test_build_record():
+def test_build_record(): # this doesn't work because "status" seems to be flaky (maybe due to http request on pdf_check()
     assert oar.build_record(item) == {'title': 'Improved method for high efficiency transformation of intact yeast cells', 'pubdate': '1992-01-01', 'doi': 'https://doi.org/10.1093/nar/20.6.1425', 'authors': ['Daniel Gietz', 'Andrew St. Jean', 'Robin A. Woods', 'Robert H. Schiestl'], 'type': 'article', 'keywords': ['biology', 'cell biology', 'computational biology', 'gene', 'genetics', 'saccharomyces cerevisiae', 'transformation (genetics)', 'yeast'], 'license': 'no license', 'pdf_url': 'https://europepmc.org/articles/pmc312198?pdf=render', 'status': 'flagged'}
